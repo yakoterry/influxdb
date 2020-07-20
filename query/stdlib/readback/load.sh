01604ce7608f0db4325137c00936f757249ebd54
Adrian Thurston <thurston@colm.net>
2020-07-20 09:53:45 +0300

    test(readback): include the load.sh as a checked-in file

---
+++ query/stdlib/readback/load.sh
@@ -0,0 +1,26 @@
+# How many test case processes to run in parallel.
+PARALLEL=16
+
+# How many iterations of of parallel runs to make.
+ITERATIONS=16
+
+logfn() {
+	printf "log-%02d-%02d.txt" $1 $2
+}
+
+for run in `seq 1 $ITERATIONS`; do
+	for i in `seq 1 $PARALLEL`; do
+		LOG=`logfn $run $i`
+		./readback.test >$LOG &
+		sleep 0.5
+	done
+
+	for i in `seq 1 $PARALLEL`; do
+			wait
+	done
+
+	for i in `seq 1 $PARALLEL`; do
+		LOG=`logfn $run $i`
+		grep -q ^FAIL $LOG || rm $LOG 
+	done
+done
